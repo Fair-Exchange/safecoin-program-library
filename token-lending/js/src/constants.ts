73de7b3d719bc9b8cc6c953161ac80e8a1a17622
Charles <charles.eubanks@gmail.com>
2021-12-02 14:57:14 -0800

    migrate from Account to Keypair
    set correct id for token-swap program
    
    update safe-token package version
    
    update web3.js package version

--- token-lending/js/src/constants.ts
+++ token-lending/js/src/constants.ts
@@ -1,7 +1,7 @@
 import { PublicKey } from '@safecoin/web3.js';
 import BigNumber from 'bignumber.js';
 
-export const LENDING_PROGRAM_ID = new PublicKey('6TvznH3B2e3p2mbhufNBpgSrLx6UkgvxtVQvopEZ2kuH');
+export const LENDING_PROGRAM_ID = new PublicKey('LENeX3L4CE1euBZp4zUNuicLP2SUZCbgXYZgBpZ9hWZ');
 
 /** @internal */
 export const ORACLE_PROGRAM_ID = new PublicKey('5mkqGkkWSaSk2NL9p4XptwEQu4d5jFTJiurbbzdqYexF');
