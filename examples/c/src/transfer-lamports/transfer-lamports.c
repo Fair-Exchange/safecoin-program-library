beeea2e5bde2849afa16b1c4d5bc4e6f6392a85f
jeffowy <jjgallow@gmail.com>
2022-07-23 14:22:03 -0600

    1.10.32

---
+++ examples/c/src/transfer-lamports/transfer-lamports.c
@@ -0,0 +1,32 @@
+/**
+ * @brief A program demonstrating the transfer of lamports
+ */
+#include <safecoin_sdk.h>
+
+extern uint64_t transfer(SafeParameters *params) {
+  // As part of the program specification the first account is the source
+  // account and the second is the destination account
+  if (params->ka_num != 2) {
+    return ERROR_NOT_ENOUGH_ACCOUNT_KEYS;
+  }
+  SafeAccountInfo *source_info = &params->ka[0];
+  SafeAccountInfo *destination_info = &params->ka[1];
+
+  // Withdraw five lamports from the source
+  *source_info->lamports -= 5;
+  // Deposit five lamports into the destination
+  *destination_info->lamports += 5;
+
+  return SUCCESS;
+}
+
+extern uint64_t entrypoint(const uint8_t *input) {
+  SafeAccountInfo accounts[2];
+  SafeParameters params = (SafeParameters){.ka = accounts};
+
+  if (!sol_deserialize(input, &params, SAFE_ARRAY_SIZE(accounts))) {
+    return ERROR_INVALID_ARGUMENT;
+  }
+
+  return transfer(&params);
+}
---
+++ utils/test-client/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let sdk_dep = safecoin_sdk::signature::Signature::default();
+    println!("Yes have some sdk_dep {:?}", sdk_dep);
+    let memo_dep = safe_memo::id();
+    println!("Yes have some memo_dep {:?}", memo_dep);
+    let token_dep = safe_token::id();
+    println!("Yes have some token_dep {:?}", token_dep);
+    let token_swap_dep = safe_token_swap::id();
+    println!("Yes have some token_swap_dep {:?}", token_swap_dep);
+}
